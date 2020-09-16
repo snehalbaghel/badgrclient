13f2ff5d282cb1d4e30f7fbc9bf2ae03084ec1ac
Snehal Baghel <snehalbaghel@gmail.com>
2020-09-16 18:56:26 +0530

    update LICENSE

--- setup.py
+++ setup.py
@@ -6,32 +6,31 @@ with open("README.md", "r") as fh:
 
 def get_requires(test=None):
     if test:
-        req_file = 'test-requirements.txt'
+        req_file = "test-requirements.txt"
     else:
-        req_file = 'requirements.txt'
+        req_file = "requirements.txt"
 
-    with open(req_file, 'r') as f:
+    with open(req_file, "r") as f:
         return [line.strip() for line in f]
 
 
 setuptools.setup(
-    name='badgrclient',
-    packages=['badgrclient'],
-    version='0.1',
-    description='A python library for Badgr APIs',
-    author='Snehal Baghel',
-    author_email='snehalbaghel@gmail.com',
-    url='https://github.com/snehalbaghel/badgrclient',
-    keywords=['badgr', 'openbadges', 'api', 'library'],
+    name="badgrclient",
+    packages=["badgrclient"],
+    version="0.1",
+    description="A python library for Badgr APIs",
+    author="Snehal Baghel",
+    author_email="snehalbaghel@gmail.com",
+    url="https://github.com/snehalbaghel/badgrclient",
+    keywords=["badgr", "openbadges", "api", "library"],
     classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: GNU General Public License v2 '
-        'or later (GPLv2+)',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
     ],
-    license='GNU General Public License v2.0',
+    license="GNU General Public License v3.0",
     install_requires=get_requires(),
-    test_requires=get_requires(test=True)
+    test_requires=get_requires(test=True),
 )
