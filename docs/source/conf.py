cf62f50fbb2ad400c19d9dc99721fd68170f2088
Snehal Baghel <snehalbaghel@gmail.com>
2020-10-02 15:48:45 +0530

    Add github link to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,19 @@ exclude_patterns = [
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+    'description': 'A python library for Badgr APIs',
+    'show_powered_by': False,
+    'github_user': 'snehalbaghel',
+    'github_repo': 'badgrclient',
+    'github_banner': True,
+    'show_related': False,
+    'note_bg': '#FFF59C',
+    'github_button': True,
+    'github_type': 'star',
+    'page_width': '1024px',
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
