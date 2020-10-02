0646f4b20a539d1ff02030cbf4de6a925ac9bcbb
Snehal Baghel <snehalbaghel@gmail.com>
2020-10-02 15:14:56 +0530

    add master_doc to conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Snehal Baghel'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
